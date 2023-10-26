cced4498f75f3d7afa81ebcf4314424d23b8a077
Nayan Rathod <rnayan2709@gmail.com>
2023-10-25 21:51:31 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "25px";
+//     } else if (document.body.scrollTop > 45 || document.documentElement.scrollTop > 45) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "26px";
+//     } else if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "27px";
+//     } else if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "28px";
+//     } else if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "29px";
+//     } else if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "30px";
+//     } else if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "31px";
+//     } else if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "32px";
+//     } else if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "33px";
+//     } else if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "34px";
+//     } else {
+//         document.getElementById("navbar").style.padding = "10px 10px";
+//         document.getElementById("logo").style.fontSize = "35px";
+//     }
+// }
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 1)";
+//     } else if (document.body.scrollTop > 45 || document.documentElement.scrollTop > 45) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0.9)";
+//     } else if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0.8)";
+//     } else if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0.7)";
+//     } else if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0.6)";
+//     } else if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0.5)";
+//     } else if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0.4)";
+//     } else if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0.3)";
+//     } else if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0.2)";
+//     } else if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0.1)";
+//     } else {
+//         document.getElementById("navbar").style.backgroundColor = "rgba(43, 45, 66, 0)";
+//     }
+// }
